8c1c3b728b6b3a0c93fdc2aff52879e57aca6c73
Abdi <39010800+yaudahbanh@users.noreply.github.com>
2023-05-25 14:34:30 +0700

    Add files via upload

---
+++ Introduction/ASCII/ascii.py
@@ -0,0 +1,5 @@
+# Solver ASCII Introduction Cryptohack.org
+
+enc = [99, 114, 121, 112, 116, 111, 123, 65, 83, 67, 73, 73, 95, 112, 114, 49, 110, 116, 52, 98, 108, 51, 125]
+
+print("".join(chr(pale) for pale in enc))
