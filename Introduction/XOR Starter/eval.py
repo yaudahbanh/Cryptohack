dd8891c8c553a152ae45f17ab6ffa5195de3cf38
Abdi <39010800+yaudahbanh@users.noreply.github.com>
2023-05-25 15:32:34 +0700

    Add files via upload

---
+++ Introduction/XOR Starter/eval.py
@@ -0,0 +1,11 @@
+# Evaluation about XOR Started challenge as Introduction on Cryptohack.org
+
+string = "aloha"
+integer = 13
+
+a = [ord(pale) for pale in string]
+b = [integer ^ i for i in a]
+
+c = "".join(chr(okok) for okok in b)
+
+print(c)
---
+++ Introduction/XOR Starter/xored.py
@@ -0,0 +1,11 @@
+# Solver XOR Starter Introduction Cryptohack.org
+
+string = "label"
+integer = 13
+
+a = [ord(pale) for pale in string]
+b = [integer ^ i for i in a]
+
+c = "".join(chr(okok) for okok in b)
+
+print("crypto{" + c + "}")
